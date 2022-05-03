e4b462b7e9b615475c04978d14fd8b195d57f0ef
Alexandre Da Silva <adasilva@topics.fr>
2022-05-03 09:22:48 +0200

    [add] ajout de 2 fonctions

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\PartenairesRepository;
 use App\Repository\PresentationRepository;
 use App\Repository\PrestationsRepository;
+use App\Repository\SousPrestationsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,17 +13,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class MenuController extends AbstractController
 {
     #[Route('/menu', name: 'creer_menu')]
-    public function creer_menu( PresentationRepository $presentationRepository, PartenairesRepository $partenairesRepository,  PrestationsRepository $prestationsRepository): Response
+    public function creer_menu(SousPrestationsRepository $sousPrestationsRepository ,PresentationRepository $presentationRepository, PartenairesRepository $partenairesRepository,  PrestationsRepository $prestationsRepository): Response
     {   
         $presentations = $presentationRepository->findAll();
         $partanires = $partenairesRepository->findAll();
         $prestations = $prestationsRepository->findAll();
-
+        $sousPresta = $sousPrestationsRepository->findAll();
         return $this->render('menu/index.html.twig', [
             'presentations' => $presentations,
             'partanires' => $partanires,
             'prestations' => $prestations,
-
+            'sousPresta' => $sousPresta,
+            
         ]);
     }
 }
